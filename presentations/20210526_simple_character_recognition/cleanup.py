a6ffd80871e0a078082e2bee933abfe8f4f33adb
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-05-23 14:44:30 -0700

    update

--- presentations/20210526_simple_character_recognition/cleanup.py
+++ presentations/20210526_simple_character_recognition/cleanup.py
@@ -5,7 +5,6 @@ def run(c): # run something at terminal and wait to finish
     print(c)
     a = os.system(c)
 
-run('rm *.png')
-run('rm test* truth*')
-run('rm -rf test truth')
-
+run('rm *.png')  # remove figures
+run('rm test* truth*')  # wipe test and truth data
+run('rm -rf test truth')  # wipe test and truth data segment files

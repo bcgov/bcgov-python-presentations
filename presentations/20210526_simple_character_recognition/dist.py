bc51a0ae0eeffb6c5ea1163a5f6a29b0171e7581
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-05-24 20:11:50 -0700

    update

--- presentations/20210526_simple_character_recognition/dist.py
+++ presentations/20210526_simple_character_recognition/dist.py
@@ -18,33 +18,34 @@ def normalize(A):  # normalize a list of [x,y] points.. subtract centroid
 
 def dist(X, Y):
     rho, [x1, y1], [x2, y2] = 0, X, Y  # compare two point-sets
-    i_f_n, j_f_n = len(x1), len(x2)
+    i_n, j_n = len(x1), len(x2)
     subdist, arrows = [], []  # for visualization only
 
     # element-wise slots for inclusion of each element in traffic plan
-    i_f, j_f = [False for i in range(len(x1))], [False for i in range(len(x2))]
+    i_f, j_f = [False for i in range(i_n)], [False for i in range(j_n)]
 
     dm = [[abs(x1[i] - x2[j]) + abs(y1[i] - y2[j]), i, j]
-          for i in range(len(x1)) for j in range(len(x2))]  # distance matrix
+          for i in range(i_n) for j in range(j_n)]  # distance matrix
     dm.sort()  # sorted distance matrix
 
     for k in range(len(dm)):  # for each dmat element
         d, i, j = dm[k]  # dist btwn points, indices of points compared
 
         if not i_f[i] and not j_f[j]:  # if slots open for both elements:
+
             # add term to distance and close the slots
-            i_f[i], i_f_n, j_f[j], j_f_n, rho = [True,
-                                                 i_f_n - 1,
+            i_f[i], i_n, j_f[j], j_n, rho = [True,
+                                                 i_n - 1,
                                                  True,
-                                                 j_f_n - 1,
+                                                 j_n - 1,
                                                  rho + d]
 
             if d > 0:  # record stuff for visualization
                 arrows.append([[x1[i], y1[i]], [x2[j], y2[j]]])
                 subdist.append(d)
 
-        if i_f_n * j_f_n == 0:
+        if i_n * j_n == 0:
             break  # ran out of slots for X or Y: stop comparing..
 
-    rho /= min(float(len(x1)), float(len(x2)))  # divide by number of slots
+    rho /= len(arrows)  # divide by the number of slots
     return rho, arrows, subdist

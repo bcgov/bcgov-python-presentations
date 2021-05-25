c6be5080d16d442f38a0911488283e55ba2c1249
Ashlin Richardson <richardson.ashlin@gmail.com>
2021-05-24 20:33:47 -0700

    pep8

--- presentations/20210526_simple_character_recognition/flood.py
+++ presentations/20210526_simple_character_recognition/flood.py
@@ -1,13 +1,16 @@
-def flood(img, i, j, my_label = None, my_color = None): # flood-fill segmentation
-    if i >= img.rows or j >= img.cols or i < 0 or j < 0: return  # out of bounds
-    ix = i * img.cols + j # linear index of (i, j)
-    # print("i", i, "j", j, "ix", ix, "rows", img.rows, "cols", img.cols)
+def flood(img, i, j, my_label=None, my_color=None):  # flood-fill segmentation
+    if i >= img.rows or j >= img.cols or i < 0 or j < 0:
+        return  # out of bounds
+    ix = i * img.cols + j  # linear index of (i, j)
 
-    if img.labels[ix] > 0: return # stop: already labelled
+    if img.labels[ix] > 0:
+        return  # stop: already labelled
     c_s = str(img.rgb[ix])
-    if c_s == img.max_color: return # stop: ignore background "background subtraction"
-    if my_color and my_color != c_s: return # stop: different colour than at invocation chain start
- 
+    if c_s == img.max_color:
+        return  # stop: ignore background "background subtraction"
+    if my_color and my_color != c_s:
+        return  # stop: different colour than at invocation chain start
+
     if my_label:
         img.labels[ix] = my_label
     else:
@@ -18,4 +21,3 @@ def flood(img, i, j, my_label = None, my_color = None): # flood-fill segmentatio
         for dj in [-1, 0, 1]:
             if not (di == 0 and dj == 0):
                 flood(img, i + di, j + dj, img.labels[ix], c_s)
-
